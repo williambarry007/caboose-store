6117e8f24b777b371e668f777ed60645ecad8af3
Liam Butterworth <wbbutterworth@gmail.com>
2014-02-27 15:23:54 -0600

    Fix finalize

--- app/controllers/caboose_store/checkout_controller.rb
+++ app/controllers/caboose_store/checkout_controller.rb
@@ -144,14 +144,17 @@ module CabooseStore
       # Make sure they didn't come to the page twice
       redirect_to '/' and return if @order.line_items.count == 0
       
-      # Notify the customer
-      OrdersMailer.customer_new_order(@order).deliver
+      if @order.authorized?
+        
+        # Notify the customer
+        OrdersMailer.customer_new_order(@order).deliver
       
-      # Notify the fulfillment center
-      OrdersMailer.fulfillment_new_order(@order).deliver
+        # Notify the fulfillment center
+        OrdersMailer.fulfillment_new_order(@order).deliver
       
-      # Clear everything
-      session[:cart_id] = nil
+        # Clear everything
+        session[:cart_id] = nil
+      end
     end
   end
 end
