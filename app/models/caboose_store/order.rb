ab9e8b8d1d5ece403229c9608f735de2991d2cf5
Liam Butterworth <wbbutterworth@gmail.com>
2014-03-04 15:44:33 -0600

    Minor update

--- app/models/caboose_store/order.rb
+++ app/models/caboose_store/order.rb
@@ -77,7 +77,7 @@ module CabooseStore
     def calculate_total
       self.calculate_discount
       
-      self.total = self.subtotal
+      self.total  = self.subtotal
       self.total += self.tax if self.tax
       self.total += self.shipping if self.shipping
       self.total += self.handling if self.handling
