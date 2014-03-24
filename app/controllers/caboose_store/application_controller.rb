71bb3b0cd91e4dcc5725967ea51131e6d28eaf19
Liam Butterworth <wbbutterworth@gmail.com>
2014-03-24 14:25:28 -0500

    Remove log and fix conditional

--- app/controllers/caboose_store/application_controller.rb
+++ app/controllers/caboose_store/application_controller.rb
@@ -7,13 +7,9 @@ module CabooseStore
     before_filter :init_cart
     
     def init_cart
-      ap request.env['PATH_INFO']
-      ap request.env['PATH_INFO'].split('/')
-      ap request.env['PATH_INFO'].split('/').first == 'checkout'
-      ap '----------'
       
       # Primarily for the relay, check for Order ID coming back
-      session[:cart_id] = params[:order_id] if params[:order_id]# and request.env['PATH_INFO'].split('/').first == 'checkout'
+      session[:cart_id] = params[:order_id] if params[:order_id] and request.env['PATH_INFO'].include?('checkout')
       
       # Exit the function if the Cart ID is defined and a corresponding order exists in the database
       return if session[:cart_id] and CabooseStore::Order.exists?(session[:cart_id])
