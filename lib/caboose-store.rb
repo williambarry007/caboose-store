06338d7a1c78716b8039abecc8b6aed63b7b19cb
Liam Butterworth <wbbutterworth@gmail.com>
2014-03-27 15:39:17 -0500

    Add contact email

--- lib/caboose-store.rb
+++ lib/caboose-store.rb
@@ -1,6 +1,7 @@
 require "caboose-store/engine"
 
 module CabooseStore
-  mattr_accessor :root_url, :payment_processor, :api_key, :smtp_settings, :shipping, :fulfillment_email, :shipping_email
+  mattr_accessor :root_url, :payment_processor, :api_key, :smtp_settings, :shipping,
+    :fulfillment_email, :shipping_email, :contact_email
 end
 
