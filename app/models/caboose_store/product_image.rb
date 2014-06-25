94e2b4645c412ab81ec3d6ffbfbab702469cafbc
Liam Butterworth <wbbutterworth@gmail.com>
2014-06-25 13:41:30 -0500

    Fix miscalled helper

--- app/models/caboose_store/product_image.rb
+++ app/models/caboose_store/product_image.rb
@@ -18,15 +18,15 @@ module CabooseStore
       :square_scale_factor
     
     has_attached_file :image,
-      :path => "products/:product_id_:id_:style.:extension",
+      :path        => "products/:product_id_:id_:style.:extension",
       :default_url => '/products/:id_:style.:extension',
-      :styles => {
+      :styles      => {
         tiny:   '100x100>',
         thumb:  '250x250>',
         medium: '400x400>',
         large:  '800x800>',
         huge:   '1200x1200>'
-      }  
+      }
     
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     
