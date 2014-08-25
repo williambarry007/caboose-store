35c0a7239e986ac44eeb482c1be9fdbf9a765695
William Barry <williambarry007@gmail.com>
2014-08-25 10:41:31 -0500

    More changes

--- app/controllers/caboose_store/products_controller.rb
+++ app/controllers/caboose_store/products_controller.rb
@@ -346,7 +346,7 @@ module CabooseStore
             'sku'               ,                        
             'barcode'           , 
             'price'             ,
-            'quantity' ,
+            'quantity_in_stock' ,
             'weight'            , 
             'length'            , 
             'width'             , 
