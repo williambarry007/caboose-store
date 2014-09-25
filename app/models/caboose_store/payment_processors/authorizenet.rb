2e10977c48f29969aa431cdc90cc25444aaf195a
Liam Butterworth <wbbutterworth@gmail.com>
2014-09-25 15:22:38 -0500

    Enable test gateway for production testing

--- app/models/caboose_store/payment_processors/authorizenet.rb
+++ app/models/caboose_store/payment_processors/authorizenet.rb
@@ -3,11 +3,11 @@ class CabooseStore::PaymentProcessors::Authorizenet < CabooseStore::PaymentProce
   end
   
   def self.form_url(order=nil)
-    if Rails.env == 'development'
-      'https://test.authorize.net/gateway/transact.dll'
-    else
-      'https://secure.authorize.net/gateway/transact.dll'
-    end
+    #if Rails.env == 'development'
+    'https://test.authorize.net/gateway/transact.dll'
+    #else
+    #  'https://secure.authorize.net/gateway/transact.dll'
+    #end
   end
   
   def self.authorize(order, params)
